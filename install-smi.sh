67fd3d6a09c194753451b5b1e270baca8ad60430
KnockingBrothers <37942350+KnockingBrothers@users.noreply.github.com>
2018-03-31 02:16:51 +0200

    Update install-smi.sh

--- install-smi.sh
+++ install-smi.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+echo "Debian 9  S.M.I installation "
+echo "Installation des outils Serveur "
+apt-get install mc openssh-server net-tools -y
+apt-get install build-essential -y
+apt-get install libxml2-dev libcurl4-openssl-dev libjpeg-dev libpng-dev libxpm-dev libpq-dev libicu-dev libfreetype6-dev libldap2-dev libfcgi-dev libfcgi0ldbl libmcrypt-dev libssl-dev -y
+
 clear
 echo "This the End "
 echo "That's all folks"
